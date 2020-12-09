47d86f08fa946753ac6b5503043b771f0cea3d0d
垚夫 庞 <349332929@qq.com>
2020-12-09 13:54:19 -0800

    upload Towers of Hanoi

--- Towers of Hanoi/windows32/example.asm
+++ Towers of Hanoi/windows32/example.asm
@@ -1,4 +1,3 @@
-; Example assembly language program -- adds 158 to number in memory
 ; Author:  MingkuanPang
 ; Date:    10/8/2020    
 
@@ -32,10 +31,62 @@ _MainProc    PROC
 	ret
 _MainProc    ENDP
 
-fctn1   PROC
+;mov(num,'A','B','C')
+move   PROC
 	push	ebp
 	mov		ebp,esp
+	push	eax
+	push	ebx
+	mov		eax,[ebp+8]
+	cmp		eax,1
+	jne		moreThanOne
+	mov		ebx,[ebp+12]
+	mov		source,bl
+	mov		ebx,[ebp+20]
+	mov		dest,bl
+	output	outLbl,outMsg
+	jmp		finishMoving
 
+	moreThanOne:
+	;mov	n-1 disks	from A to B move(n-1,A,C,B)
+	dec		eax
+	mov		bl,[ebp+16]
+	push	ebx
+	mov		bl,[ebp+20]
+	push	ebx
+	mov		bl,[ebp+12]
+	push	ebx
+	push	eax
+	call	move
+	add		esp,16
+
+	;mov	bottom disk	from A to C move(1,A,B,C)
+	mov		bl,[ebp+20]
+	push	ebx
+	mov		bl,[ebp+16]
+	push	ebx
+	mov		bl,[ebp+12]
+	push	ebx
+	mov		ebx,1
+	push	ebx
+	call	move
+	add		esp,16
+
+	;mov	n-1 disk from B to C mov(n-1,B,A,C)
+	mov		bl,[ebp+20]
+	push	ebx
+	mov		bl,[ebp+12]
+	push	ebx
+	mov		bl,[ebp+16]
+	push	ebx
+	push	eax
+	call	move
+	add		esp,16
+
+	finishMoving:
+	pop		ebx
+	pop		eax
+	pop		ebp
 	ret
-fctn1   ENDP
+move   ENDP
 END                           
--- win32/windows32/example.asm
+++ win32/windows32/example.asm
@@ -1,28 +1,92 @@
-;some SSE instructions
 ; Author:  MingkuanPang
-; Date:    12/6/2020    
+; Date:    10/8/2020    
 
 .586
 .MODEL FLAT
-INCLUDE io.h
+ INCLUDE io.h
 .STACK  4096            ; reserve 4096-byte stack
-.DATA	; reserve storage for data
-a		REAL4	2.0
-b		REAL4	-1.0
-c		REAL4	-15.0
-discr	REAL4	?
-four	REAL4	4.0
+
+.DATA                   ; reserve storage for data
+prompt	BYTE	"How many disks?",0
+number	BYTE	16 DUP(?)
+outLbl	BYTE	"Move disk",0
+outMsg	BYTE	"from spindle "
+source	BYTE	?, 0ah,	0dh
+		BYTE	'to spindle '
+dest	BYTE	?, 0
 .CODE       
 _MainProc    PROC
-	movss	xmm0,b	;copy b
-	mulss	xmm0,xmm0	;b*b
-	movss	xmm1,a
-	mulss	xmm1,four
-	mulss	xmm1,c
-	subss	xmm0,xmm1
-	sqrtss	xmm0,xmm0
-	movss	discr,xmm0
-	mov			eax,0
+	mov		al,'C'
+	push	eax
+	mov		al,'B'
+	push	eax
+	mov		al,'A'
+	push	eax
+	input	prompt, number, 16
+	atod	number
+	push	eax
+	call	move
+	add		esp,16
+	mov		eax,0
 	ret
 _MainProc    ENDP
-END
+
+;mov(num,'A','B','C')
+move   PROC
+	push	ebp
+	mov		ebp,esp
+	push	eax
+	push	ebx
+	mov		eax,[ebp+8]
+	cmp		eax,1
+	jne		moreThanOne
+	mov		ebx,[ebp+12]
+	mov		source,bl
+	mov		ebx,[ebp+20]
+	mov		dest,bl
+	output	outLbl,outMsg
+	jmp		finishMoving
+
+	moreThanOne:
+	;mov	n-1 disks	from A to B move(n-1,A,C,B)
+	dec		eax
+	mov		bl,[ebp+16]
+	push	ebx
+	mov		bl,[ebp+20]
+	push	ebx
+	mov		bl,[ebp+12]
+	push	ebx
+	push	eax
+	call	move
+	add		esp,16
+
+	;mov	bottom disk	from A to C move(1,A,B,C)
+	mov		bl,[ebp+20]
+	push	ebx
+	mov		bl,[ebp+16]
+	push	ebx
+	mov		bl,[ebp+12]
+	push	ebx
+	mov		ebx,1
+	push	ebx
+	call	move
+	add		esp,16
+
+	;mov	n-1 disk from B to C mov(n-1,B,A,C)
+	mov		bl,[ebp+20]
+	push	ebx
+	mov		bl,[ebp+12]
+	push	ebx
+	mov		bl,[ebp+16]
+	push	ebx
+	push	eax
+	call	move
+	add		esp,16
+
+	finishMoving:
+	pop		ebx
+	pop		eax
+	pop		ebp
+	ret
+move   ENDP
+END                           
